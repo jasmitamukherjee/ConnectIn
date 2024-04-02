03db6b4ec428fa12d0e0a890b614a7cbe2a0d2d2
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-04-03 00:38:18 +0530

    Update EmployerHome.js

--- screens/Profile/Employer/EmployerHome.js
+++ screens/Profile/Employer/EmployerHome.js
@@ -9,11 +9,8 @@ import axios from 'axios';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import Entypo from 'react-native-vector-icons/Entypo'
 import Foundation from 'react-native-vector-icons/Foundation'
-import Octicons from 'react-native-vector-icons/Octicons'
-
-import MaterialCommunityIcons
-from 'react-native-vector-icons/MaterialCommunityIcons'
-
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 const EmployerHome = () => {
   const [option,setOption] = useState("Compatible")
   const [employerId,setEmployerId] = useState("");
@@ -63,6 +60,7 @@ const EmployerHome = () => {
   console.log("Profiles:",currentEmployerProfile)
 
   return (
+    <>
     <ScrollView style={{marginTop:25}}>
 <View  style={{
             padding: 10,
@@ -187,16 +185,17 @@ const EmployerHome = () => {
     <View style={{backgroundColor:"white",padding:10,borderRadius:8}}>
       <View style={{flexDirection:"row",paddingTop:5,alignItems:"center",gap:20,borderBottomWidth:0.8,borderBottomColor:"gray",paddingBottom:10}}>
         <View style={{flexDirection:"row",alignItems:"center",gap:10}}>
-        <Ionicons
- name="person" size={30} color="#452c63"/>
- <Text style={{fontFamily:"monospace",color:"black"}}>{currentEmployerProfile?.gender}</Text>
+        <MaterialCommunityIcons
+
+ name="star-circle" size={30} color="#452c63"/>
+ <Text style={{fontFamily:"monospace",color:"black",fontWeight:"bold"}}>{currentEmployerProfile?.gender}</Text>
 
         </View>
 
         <View style={{flexDirection:"row",alignItems:"center",gap:10}}>
         <Ionicons
  name="magnet" size={30} color="#452c63"/>
- <Text style={{fontFamily:"monospace",color:"black"}}>{currentEmployerProfile?.userType}</Text>
+ <Text style={{fontFamily:"monospace",color:"black",fontWeight:"bold"}}>{currentEmployerProfile?.userType}</Text>
 
         </View>
 
@@ -204,7 +203,7 @@ const EmployerHome = () => {
         <Entypo
 
  name="home" size={30} color="#452c63"/>
- <Text style={{fontFamily:"monospace",color:"black"}}>{currentEmployerProfile?.location}</Text>
+ <Text style={{fontFamily:"monospace",color:"black",fontWeight:"bold"}}>{currentEmployerProfile?.location}</Text>
 
         </View>
 
@@ -213,15 +212,219 @@ const EmployerHome = () => {
         
       </View>
 
+
+      <View style={{marginVertical:15}}>
+        <View>
+          {currentEmployerProfile?.imageUrls?.length>1 && (
+            <View>
+              <Image source={{uri:currentEmployerProfile.imageUrls[1]}} style={{width:"100%",height:350,resizeMode:"cover",borderRadius:10}}/>
+              <Pressable 
+              style={{position:"absolute",bottom:10,right:10,backgroundColor:"white",width:42,height:42,borderRadius:21,justifyContent:"center",alignItems:"center"}}>
+              <Foundation name="like" size={30} color="#452c63"/>
+
+              </Pressable>
+            </View> 
+
+            
+            
+          )}
+        </View>
+
+    </View>
+
+      <View style={{
+  marginTop: 15,
+  borderBottomWidth: 0.8,
+  borderBottomColor: '#E0E0E0',
+  paddingBottom: 10,
+}}>
+  <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
+    <Ionicons name="glasses" size={30} color="#452c63" />
+    <Text style={{ marginLeft: 10, fontSize: 15, fontFamily: "monospace", color: "black",fontWeight:"bold" }}>My job preferences are:</Text>
+  </View>
+  <ScrollView horizontal>
+    {currentEmployerProfile && currentEmployerProfile.lookingFor ? (
+      <Text style={{ fontSize: 16, fontFamily: "monospace", color: "black" ,fontWeight:"bold"}}>
+        {currentEmployerProfile.lookingFor.join(", ")}
+      </Text>
+    ) : (
+      <Text style={{ fontSize: 16, fontFamily: "monospace" }}>Open to all kinds of jobs.</Text>
+    )}
+  </ScrollView>
+</View>
+<View style={{marginVertical:15}}>
+        <View>
+          {currentEmployerProfile?.imageUrls?.length>2 && (
+            <View>
+              <Image source={{uri:currentEmployerProfile.imageUrls[2]}} style={{width:"100%",height:350,resizeMode:"cover",borderRadius:10}}/>
+              <Pressable 
+              style={{position:"absolute",bottom:10,right:10,backgroundColor:"white",width:42,height:42,borderRadius:21,justifyContent:"center",alignItems:"center"}}>
+              <Foundation name="like" size={30} color="#452c63"/>
+
+              </Pressable>
+            </View> 
+
+            
+            
+          )}
+        </View>
+
+    </View>
+
+<View style={{
+  marginTop: 15,
+  borderBottomWidth: 0.8,
+  borderBottomColor: '#E0E0E0',
+  paddingBottom: 10,
+}}>
+  <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
+    <FontAwesome5 name="user-tie" size={30} color="#452c63" />
+    <Text style={{ marginLeft: 10, fontSize: 15, fontFamily: "monospace", color: "black", fontWeight: "bold" }}>My skills are:</Text>
+  </View>
+  <ScrollView horizontal={false}>
+    {currentEmployerProfile && currentEmployerProfile.skillsScreen ? (
+      currentEmployerProfile.skillsScreen.map((skill, index) => (
+        <Text key={index} style={{ fontSize: 16, fontFamily: "monospace", color: "black", fontWeight: "bold" }}>
+
+          -{skill}
+        </Text>
+      ))
+    ) : (
+      <Text style={{ fontSize: 16, fontFamily: "monospace" }}>Like to know about them.</Text>
+    )}
+  </ScrollView>
+</View>
+
+
+
+
+
+
+
      
 
     </View>
+  
+    <View style={{marginVertical:15}}>
+        <View>
+          {currentEmployerProfile?.imageUrls?.length>3 && (
+            <View>
+              <Image source={{uri:currentEmployerProfile.imageUrls[3]}} style={{width:"100%",height:350,resizeMode:"cover",borderRadius:10}}/>
+              <Pressable 
+              style={{position:"absolute",bottom:10,right:10,backgroundColor:"white",width:42,height:42,borderRadius:21,justifyContent:"center",alignItems:"center"}}>
+              <Foundation name="like" size={30} color="#452c63"/>
+
+              </Pressable>
+            </View> 
+
+            
+            
+          )}
+        </View>
+
+    </View>
+    <View style={{marginVertical:15}}>
+      {currentEmployerProfile?.prompts?.length > 0 && 
+      (   currentEmployerProfile?.prompts.slice(1,2).map((prompt,index) => {
+        <>
+        <View style={{backgroundColor:"white",padding:12,borderRadius:10,height:150,justifyContent:"center"}}  key={index}>
+          <Text style={{color:"black",fontSize:15,fontWeight:"500",fontFamily:"monospace"}}>
+            {prompt.question}
+
+
+          </Text>
+        <Text style={{color:"black",fontSize:20,fontWeight:"600",fontFamily:"monospace",marginTop:20}}>
+          {prompt.answer}
+
+        </Text>
+        </View>
+        <Pressable 
+              style={{position:"absolute",bottom:10,right:10,backgroundColor:"white",width:42,height:42,borderRadius:21,justifyContent:"center",alignItems:"center",
+              shadowColor:"#000",
+              shadowOpacity: 0.25,
+              shadowRadius:3.84
+              }}>
+              <Foundation name="like" size={30} color="#452c63"/>
+
+              </Pressable>
+        </>
+      }))
+    }
+
+    </View>
+
+    <View style={{marginVertical:15}}>
+        <View>
+          {currentEmployerProfile?.imageUrls?.length>4 && (
+            <View>
+              <Image source={{uri:currentEmployerProfile.imageUrls[4]}} style={{width:"100%",height:350,resizeMode:"cover",borderRadius:10}}/>
+              <Pressable 
+              style={{position:"absolute",bottom:10,right:10,backgroundColor:"white",width:42,height:42,borderRadius:21,justifyContent:"center",alignItems:"center"}}>
+              <Foundation name="like" size={30} color="#452c63"/>
+
+              </Pressable>
+            </View>
+          )}
+        </View>
+
+    </View>
+    <View style={{marginVertical:15}}>
+      {currentEmployerProfile?.prompts?.length > 0 && 
+      (   currentEmployerProfile?.prompts.slice(2,3).map((prompt,index) => {
+        <>
+        <View style={{backgroundColor:"white",padding:12,borderRadius:10,height:150,justifyContent:"center"}}  key={index}>
+          <Text style={{color:"black",fontSize:15,fontWeight:"500",fontFamily:"monospace"}}>
+            {prompt.question}
+
+
+          </Text>
+        <Text style={{color:"black",fontSize:20,fontWeight:"600",fontFamily:"monospace",marginTop:20}}>
+          {prompt.answer}
+
+        </Text>
+        </View>
+        <Pressable 
+              style={{position:"absolute",bottom:10,right:10,backgroundColor:"white",width:42,height:42,borderRadius:21,justifyContent:"center",alignItems:"center",
+              shadowColor:"#000",
+              shadowOpacity: 0.25,
+              shadowRadius:3.84
+              }}>
+              <Foundation name="like" size={30} color="#452c63"/>
+
+              </Pressable>
+        </>
+      }))
+    }
+
+    </View>
+    <View style={{marginVertical:15}}>
+        <View>
+          {currentEmployerProfile?.imageUrls?.length>5 && (
+            <View>
+              <Image source={{uri:currentEmployerProfile.imageUrls[5]}} style={{width:"100%",height:350,resizeMode:"cover",borderRadius:10}}/>
+              <Pressable 
+              style={{position:"absolute",bottom:10,right:10,backgroundColor:"white",width:42,height:42,borderRadius:21,justifyContent:"center",alignItems:"center"}}>
+              <Foundation name="like" size={30} color="#452c63"/>
+
+              </Pressable>
+            </View>
+          )}
+        </View>
+
+    </View>
+    
+
     </View>
     
     </>
 
   </View>
       </ScrollView>
+      <Pressable style={{position:"absolute",bottom:15,left:12,backgroundColor:"white",width:50,height:50,borderRadius:25,justifyContent:"center",alignItems:"center"}}>
+      <Entypo name="cross" size={30} color="#452c63"/>
+
+      </Pressable>
+      </>
   )
 }
 
